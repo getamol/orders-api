fe8da99eb7a7e2ed9820e17839b08c6badbbc482
pradeep <pradeep.kumar44@gmail.com>
2022-07-16 16:08:28 +0530

    committing the changes for pagination support

--- src/main/java/com/classpath/orders/controller/OrdersRestController.java
+++ src/main/java/com/classpath/orders/controller/OrdersRestController.java
@@ -2,6 +2,8 @@ package com.classpath.orders.controller;
 
 import java.util.Map;
 import java.util.Set;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.classpath.orders.model.Order;
 import com.classpath.orders.service.OrderService;
@@ -50,11 +53,13 @@ public class OrdersRestController {
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Order saveOrder(@RequestBody Order order) {
 		return this.orderService.saveOrder(order);
 	}
 	
 	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteOrderById(@PathVariable("id") long orderId) {
 		this.orderService.deleteOrderById(orderId);
 	}
